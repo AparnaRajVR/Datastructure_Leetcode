a1bd16b6a140360dc113305732e75ac3f1953360
AparnaRajVR <aparnaraajvr@gmail.com>
2025-11-21 13:36:07 +0530

    Time: 0 ms (100.00%) | Memory: 151.3 MB (25.00%) - LeetSync

---
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.dart
@@ -0,0 +1,11 @@
+class Solution {
+  int? numberOfEmployeesWhoMetTarget(List<int> hours, int target) {
+    int count=0;
+   for(int i=0;i<=hours.length-1;i++) {
+    if(hours[i]>=target){
+    count++;
+    }
+   }
+  return count;
+  }
+}
